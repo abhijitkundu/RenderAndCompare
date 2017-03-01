18dc48bb5f029f9a43baa52a59ab23b0c7f8ba43
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-03-01 12:42:46 -0500

    added script to select and copy pascal images with cars

---
+++ data/generate_bkg_images/copy_voc_images_having_cars.py
@@ -0,0 +1,16 @@
+import os.path as osp
+import pandas as pd
+from shutil import copyfile
+
+voc_root_dir = '/media/Scratchspace/PascalVOC-Datasets/VOCdevkit2012/VOC2012'
+voc_images_dir = osp.join(voc_root_dir, 'JPEGImages')
+image_set_file = osp.join(voc_root_dir, 'ImageSets', 'Main', 'car_train.txt')
+
+df = pd.read_csv(image_set_file, delim_whitespace=True, names = ['image_name', 'presence'], header=None)
+car_images = df.loc[df['presence'] >= 0]
+car_image_names = car_images['image_name'].tolist()
+
+for image_name in car_image_names:
+    image_filename = image_name + '.jpg'
+    copyfile(osp.join(voc_images_dir, image_filename), image_filename)
+
