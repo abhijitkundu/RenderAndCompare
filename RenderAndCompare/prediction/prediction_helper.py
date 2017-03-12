e2873c49f281ab5ee96b1c1e71c06c4c71bbd6bd
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-03-11 19:55:31 -0500

    made prediction helper more generic

--- RenderAndCompare/prediction/prediction_helper.py
+++ RenderAndCompare/prediction/prediction_helper.py
@@ -57,7 +57,7 @@ def get_predictions_on_image_files(img_files, net_file, weights_file, requested_
         output = net.forward()
 
         for key in predictions:
-            predictions[key][start_idx:end_idx, :] = output[key][0:end_idx - start_idx, :]
+            predictions[key][start_idx:end_idx, ...] = output[key][0:end_idx - start_idx, ...]
 
     return predictions
 
