6cc1eeb4e12fe1859679def411dcc6731829e4a7
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-03-12 20:48:33 -0400

    Added more caffe layers and testing

--- RenderAndCompare/layers/function_layers.py
+++ RenderAndCompare/layers/function_layers.py
@@ -52,3 +52,105 @@ class SoftMaxWithTemperature(caffe.Layer):
 
     def backward(self, top, propagate_down, bottom):
         pass
+
+
+class L2Normalization(caffe.Layer):
+    def setup(self, bottom, top):
+        assert len(bottom) == 1, 'requires a single layer.bottom'
+        assert len(top) == 1, 'requires a single layer.top'
+        assert bottom[0].data.ndim == 2
+
+    def reshape(self, bottom, top):
+        top[0].reshape(*bottom[0].data.shape)
+
+    def forward(self, bottom, top):
+        sq_bottom = np.square(bottom[0].data)
+        self.sq_norm = np.sum(sq_bottom, axis=1, keepdims=True)
+        norm = np.sqrt(self.sq_norm + np.finfo(float).eps)
+        self.inv_norm = 1. / norm
+        top[0].data[...] = bottom[0].data * self.inv_norm
+
+    def backward(self, top, propagate_down, bottom):
+        # diff = top[0].diff * inv_norm
+        # dinv_norm = bottom[0].data
+        # dnorm = -1. / (norm**2) * dinv_norm
+        # dsq_norm = 0.5 * (1. / np.sqrt(sq_norm + np.finfo(float).eps)) * dnorm
+        # dsq = dsq_norm
+        # diff += 2 * bottom[0].data * dsq
+        # bottom[0].diff[...] = diff
+
+        diff = top[0].diff * self.inv_norm
+        dnorm = -1. / self.sq_norm * bottom[0].data
+        dsq_norm = 0.5 * (1. / np.sqrt(self.sq_norm + np.finfo(float).eps)) * dnorm
+        diff += 2 * bottom[0].data * dsq_norm
+        bottom[0].diff[...] = diff
+
+
+class AngularExpectation(caffe.Layer):
+    """
+    Computes expected angle from prob and bin size
+    Output is [cos, sin]
+    """
+
+    def parse_param_str(self, param_str):
+        parser = argparse.ArgumentParser(description='AngularExpectation Layer')
+        parser.add_argument("-b", "--num_of_bins", default=24, type=int, help="Number of bins")
+        params = parser.parse_args(param_str.split())
+        return params
+
+    def setup(self, bottom, top):
+        assert len(bottom) == 1, 'requires a single layer.bottom'
+        assert len(top) == 1, 'requires a single layer.top'
+
+        # params is expected as argparse style string
+        params = self.parse_param_str(self.param_str)
+
+        self.num_of_bins = params.num_of_bins
+        assert bottom[0].data.ndim == 2
+        assert bottom[0].data.shape[1] == self.num_of_bins
+        top[0].reshape(bottom[0].data.shape[0], 2)
+
+        angles = (2 * np.pi / self.num_of_bins) * (np.arange(0.5, self.num_of_bins))
+        anglesz = np.exp(1j * angles)
+        self.cs = np.hstack((np.real(anglesz).reshape(-1, 1), np.imag(anglesz).reshape(-1, 1)))
+
+        print "------------- AngularExpectation Layer Config ------------------"
+        print "Number of bins = {}".format(self.num_of_bins)
+        print "bottom.shape =   {}".format(bottom[0].data.shape)
+        print "top.shape =      {}".format(top[0].data.shape)
+        print "--------------------------------------------------------------"
+
+    def reshape(self, bottom, top):
+        pass
+
+    def forward(self, bottom, top):
+        # bottom_dot_cs = bottom[0].data.dot(self.cs)
+        # l2_norms = np.linalg.norm(bottom_dot_cs, axis=1, keepdims=True)
+        # p_dot_cs_normalized = bottom_dot_cs / l2_norms
+        top[0].data[...] = bottom[0].data.dot(self.cs)
+
+    def backward(self, top, propagate_down, bottom):
+        bottom[0].diff[...] = top[0].diff.dot(self.cs.T)
+
+
+class ConstantMultiply(caffe.Layer):
+    """A layer that just multiplies by constant user specified parame"""
+
+    def parse_param_str(self, param_str):
+        parser = argparse.ArgumentParser(description='ConstantMultiply Layer')
+        parser.add_argument("-m", "--multiplier", default=10, type=float, help="constant multiplication param")
+        params = parser.parse_args(param_str.split())
+        return params
+
+    def setup(self, bottom, top):
+        params = self.parse_param_str(self.param_str)
+        self.multiplier = params.multiplier
+
+    def reshape(self, bottom, top):
+        top[0].reshape(*bottom[0].data.shape)
+
+    def forward(self, bottom, top):
+        top[0].data[...] = self.multiplier * bottom[0].data
+
+    def backward(self, top, propagate_down, bottom):
+        bottom[0].diff[...] = self.multiplier * top[0].diff
