a298326348f866e7cd54a1392654523e2537b05b
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-05-17 11:53:31 -0400

    Added code to generate result json for articulated objects

--- RenderAndCompare/prediction/prediction_helper.py
+++ RenderAndCompare/prediction/prediction_helper.py
@@ -116,3 +116,59 @@ def get_predictions_on_image_boxes(image, boxes, net, requested_keys, mean):
             predictions[key][start_idx:end_idx, ...] = output[key][0:end_idx - start_idx, ...]
 
     return predictions
+
+
+def get_predictions_on_image_croppings(img_files, cropping_boxes, net_file, weights_file, requested_keys, mean, gpu_id):
+    assert osp.exists(net_file), 'Path to test net prototxt does not exist: {}'.format(net_file)
+    assert osp.exists(weights_file), 'Path to weights file does not exist: {}'.format(weights_file)
+    assert len(mean) == 3, 'Expects mean as list of 3 numbers ([B, G, R])'
+
+    num_of_images = len(img_files)
+
+    caffe.set_mode_gpu()
+    caffe.set_device(gpu_id)
+
+    net = caffe.Net(net_file, weights_file, caffe.TEST)
+
+    data_blob_shape = net.blobs['data'].data.shape
+
+    assert len(data_blob_shape) == 4, 'Expects 4D data blob'
+    assert data_blob_shape[1] == 3, 'Expects 2nd channel to be 3 for BGR image'
+
+    batch_size = data_blob_shape[0]
+    im_size = [data_blob_shape[3], data_blob_shape[2]]  # caffe blob are (b,c,h,w)
+
+    image_loader = BatchImageLoader(im_size)
+    image_loader.crop_and_preload_images(img_files, cropping_boxes)
+
+    predictions = {}
+    for key in requested_keys:
+        if key in net.blobs:
+            blob_shape = net.blobs[key].data.shape
+            assert blob_shape[0] == batch_size, 'Expects 1st channel to be batch_size'
+            pred_shape = list(blob_shape)
+            pred_shape[0] = num_of_images
+            predictions[key] = np.zeros(tuple(pred_shape))
+        else:
+            print 'Requested key {} not available in network'.format(key)
+
+    mean_bgr = np.array(mean).reshape(1, 3, 1, 1)
+
+    num_of_batches = int(math.ceil(num_of_images / float(batch_size)))
+    for b in xrange(num_of_batches):
+        start_idx = batch_size * b
+        end_idx = min(batch_size * (b + 1), num_of_images)
+        print 'Working on batch: %d/%d (Image# %d - %d)' % (b, num_of_batches, start_idx, end_idx)
+
+        for i in xrange(start_idx, end_idx):
+            net.blobs['data'].data[i - start_idx, ...] = image_loader[i]
+
+        # subtarct mean from image data blob
+        net.blobs['data'].data[...] -= mean_bgr
+
+        output = net.forward()
+
+        for key in predictions:
+            predictions[key][start_idx:end_idx, ...] = output[key][0:end_idx - start_idx, ...]
+
+    return predictions
---
+++ tools/predict_articulated_objects.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import _init_paths
+import RenderAndCompare as rac
+import os.path as osp
+import numpy as np
+import time
+
+if __name__ == '__main__':
+    import argparse
+    description = ('Predict shape and pose for articulated objects')
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("-n", "--net_file", required=True, help="Deploy network")
+    parser.add_argument("-w", "--weights_file", required=True, help="trained weights")
+    parser.add_argument("-m", "--mean_bgr", nargs=3, default=[103.0626238, 115.90288257, 123.15163084], type=float, metavar=('B', 'G', 'R'), help="Mean BGR color value")
+    parser.add_argument("-g", "--gpu", type=int, default=0, help="GPU Id.")
+    parser.add_argument("-d", "--dataset", required=True)
+    parser.add_argument("-o", "--output", default='results.json', help="output json filepath")
+
+    args = parser.parse_args()
+
+    print 'Loading dataset from {}'.format(args.dataset)
+    dataset = rac.datasets.Dataset.from_json(args.dataset)
+    print 'Loaded {} dataset with {} annotations'.format(dataset.name(), dataset.num_of_annotations())
+
+    image_files = []
+    cropping_boxes = []
+
+    shape_params_gt = np.zeros((dataset.num_of_annotations(), 10))
+    pose_params_gt = np.zeros((dataset.num_of_annotations(), 10))
+
+    for i in xrange(dataset.num_of_annotations()):
+        annotation = dataset.annotations()[i]
+        img_path = osp.join(dataset.rootdir(), annotation['image_file'])
+
+        visible_bbx = np.array(annotation['bbx_visible'], dtype=np.float)  # gt box (only visible path)
+        # TODO Check for bbx validity
+
+        cropping_boxes.append(visible_bbx)
+        image_files.append(img_path)
+
+        shape_params_gt[i, ...] = annotation['shape_param']
+        pose_params_gt[i, ...] = annotation['pose_param']
+
+    num_of_images = len(image_files)
+    print 'Predicting viewpoint for {} images'.format(num_of_images)
+    t0 = time.time()
+    predictions = rac.prediction.get_predictions_on_image_croppings(image_files,
+                                                                    cropping_boxes,
+                                                                    args.net_file,
+                                                                    args.weights_file,
+                                                                    ['pred_shape_target', 'pred_pose_target'],
+                                                                    args.mean_bgr,
+                                                                    args.gpu)
+
+    t1 = time.time()
+    total = t1 - t0
+    print 'Took {} seconds'.format(total)
+
+    # print shape_params_gt.shape
+    # print pose_params_gt.shape
+
+    # print predictions['pred_shape_target'].shape
+    # print predictions['pred_pose_target'].shape
+
+    mean_shape_L1 = np.mean(np.sum(np.absolute(shape_params_gt - predictions['pred_shape_target']), axis=1))
+    mean_pose_L1 = np.mean(np.sum(np.absolute(pose_params_gt - predictions['pred_pose_target']), axis=1))
+
+    print 'mean_shape_L1 = {}'.format(mean_shape_L1)
+    print 'mean_pose_L1 = {}'.format(mean_pose_L1)
+
+    for i in xrange(dataset.num_of_annotations()):
+        dataset.annotations()[i]['shape_param'] = predictions['pred_shape_target'][i, ...].tolist()
+        dataset.annotations()[i]['pose_param'] = predictions['pred_pose_target'][i, ...].tolist()
+
+    print 'Saving results at {}'.format(args.output)
+    dataset.write_data_to_json(args.output)
