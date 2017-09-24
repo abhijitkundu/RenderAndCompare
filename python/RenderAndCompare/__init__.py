e0fa5a6046108662c152ea80cc72b91dade22812
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-09-24 01:01:18 -0400

    we do not need to use imprt all submodules

--- python/RenderAndCompare/__init__.py
+++ python/RenderAndCompare/__init__.py
@@ -2,9 +2,4 @@
 RenderAndCompare module
 """
 
-import RenderAndCompare.datasets
-import RenderAndCompare.prediction
-import RenderAndCompare.geometry
-import RenderAndCompare.layers
-
 __all__ = ['datasets', 'prediction', 'geometry', 'layers']
