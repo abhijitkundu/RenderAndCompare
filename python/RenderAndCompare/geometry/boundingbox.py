ffc3f5ad58017d34e9231474a52823d341298e14
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-09-30 01:38:31 -0400

    in middle of adding fast rcnn style data layer

--- python/RenderAndCompare/geometry/boundingbox.py
+++ python/RenderAndCompare/geometry/boundingbox.py
@@ -1,55 +1,102 @@
+""" 
+BoundingBox helper
+"""
+
 import numpy as np
 
 
-class BoundingBox:
-    def __init__(self, min, size):
-        self._min = np.array([min[0], min[1]], dtype=np.float)
-        self._size = np.array([size[0], size[1]], dtype=np.float)
+def bbx_iou_overlap(bbxA, bbxB):
+    """returns a iou overlap of two bounding boxes"""
+    assert bbxA.shape == bbxB.shape
+    ndim = bbxA.size / 2
+    x1y1 = np.maximum(bbxA[:ndim], bbxB[:ndim])
+    x2y2 = np.minimum(bbxA[ndim:], bbxB[ndim:])
+
+    wh = x2y2 - x1y1
+    if np.any(wh < 0):
+        return 0
+
+    inter = wh.prod()
+    a_area = (np.asarray(bbxA[ndim:]) - np.asarray(bbxA[:ndim])).prod()
+    b_area = (np.asarray(bbxB[ndim:]) - np.asarray(bbxB[:ndim])).prod()
+    iou = inter / (a_area + b_area - inter)
+    return iou
+
+# def create_jittered_bbx(bbx, min_jittter_iou):
+#     """returns a jittered bbx which has overlap > min_jittter_iou with input bbx"""
+#     assert 0 < min_jittter_iou <= 1.0
+#     assert bbx.size % 2 == 0
+#     ndim = bbx.size / 2
+#     wh = (np.asarray(bbx[ndim:]) - np.asarray(bbx[:ndim])) * (1.0 - min_jittter_iou)
+#     assert np.all(wh >= 0)
+#     while True:
+#         jitter = (np.random.uniform(-1.0, 1.0, (2, ndim)) * wh).reshape(2*ndim, )
+#         # jitter = (np.random.normal(0.0, 0.3, (2, ndim)) * wh).reshape(2*ndim, )
+#         jittered_bbx = bbx + jitter
+#         if bbx_iou_overlap(bbx, jittered_bbx) >= min_jittter_iou:
+#             return jittered_bbx
+
+def create_jittered_bbx(bbx, min_jittter_iou):
+    """returns a jittered bbx which has overlap > min_jittter_iou with input bbx"""
+    assert 0 < min_jittter_iou <= 1.0
+    assert bbx.size % 2 == 0
+    ndim = bbx.size / 2
+    wh = (np.asarray(bbx[ndim:]) - np.asarray(bbx[:ndim])) * (1.0 - min_jittter_iou)
+    assert np.all(wh >= 0)
+    while True:
+        jitter = (np.random.uniform(-1.0, 1.0, (2, ndim)) * wh).reshape(2*ndim, )
+        # jitter = (np.random.normal(0.0, 0.3, (2, ndim)) * wh).reshape(2*ndim, )
+        jittered_bbx = bbx + jitter
+        if bbx_iou_overlap(bbx, jittered_bbx) >= min_jittter_iou:
+            return jittered_bbx
+
+
+class BoundingBox(object):
+    """Aligned BoundingBox class"""
+    def __init__(self, bbx_min, bbx_max):
+        self._min = np.array(bbx_min, dtype=np.float)
+        self._max = np.array(bbx_max, dtype=np.float)
+        assert np.all(self._min <= self._max)
 
     @classmethod
     def fromRect(cls, rect):
         'Creates BoundingBox from a rect (x, y, w, h)'
-        return cls(rect[:2], rect[2:4])
+        assert rect.size % 2 == 0
+        ndim = rect.size / 2
+        return cls(rect[:ndim], np.asarray(rect[:ndim]) + np.asarray(rect[ndim:]))
+
+    @classmethod
+    def fromArray(cls, bbx):
+        'Creates BoundingBox from a rect (x, y, w, h)'
+        assert bbx.size % 2 == 0
+        ndim = bbx.size / 2
+        return cls(bbx[:ndim], bbx[ndim:])
 
     def __repr__(self):
         'Return a nicely formatted representation string'
-        return 'BoundingBox(min=%s, size=%s, center=%s)' % (self._min, self._size, self.center())
+        return "BoundingBox(min={}, max={}, size={})".format(self._min, self._max, self.size())
 
     def min(self):
+        """returns min extent"""
         return self._min
 
     def max(self):
-        return self._min + self._size
+        """returns max extent"""
+        return self._max
 
     def center(self):
-        return self._min + self._size / 2
+        """returns bbx center"""
+        return (self._min + self._max) / 2
 
     def size(self):
-        return self._size
+        """returns bbx size"""
+        return self._max - self._min
+
+    def array(self):
+        """Returns bbx as single array [x1, y1, x2, y2] """
+        return np.concatenate((self._min, self._max))
 
     def translate(self, translation):
+        """Translate the bounding box"""
         self._min += translation
-
-
-class BoundingBoxTransform:
-    def __init__(self, translation, scale):
-        self.translation = np.array([translation[0], translation[1]], dtype=np.float)
-        self.scale = np.array([scale[0], scale[1]], dtype=np.float)
-
-    @classmethod
-    def fromTwoBoundingBoxes(cls, bbxA, bbxB):
-        'Return aTb'
-        return cls.fromNumPy((bbxB.origin - bbxA.origin) / bbxA.size, bbxB.size / bbxA.size)
-
-    def __repr__(self):
-        'Return a nicely formatted representation string'
-        return 'BoundingBox(translation=%s, scale=%s)' % (self.translation, self.scale)
-
-    def matrix(self):
-        'Returns the transformation matrix'
-        tfm = np.identity(3, dtype=np.float)
-        tfm[:2, 2] = self.translation
-#         tfm.diagonal()[:2] = self.scale
-        tfm[0, 0] = self.scale[0]
-        tfm[1, 1] = self.scale[1]
-        return tfm
+        self._max += translation
--- python/RenderAndCompare/layers/data_layers.py
+++ python/RenderAndCompare/layers/data_layers.py
@@ -1,6 +1,6 @@
 import argparse
 import os.path as osp
-from random import shuffle, random
+from random import shuffle, random, randint
 
 import numpy as np
 
@@ -275,3 +275,215 @@ class RCNNDataLayer(AbstractDataLayer):
         # subtarct mean from image data blob
         if 'input_image' in self.top_names:
             top[self.top_names.index('input_image')].data[...] -= self.mean_bgr
+
+
+class FastRCNNDataLayer(AbstractDataLayer):
+    """Data Layer Fast-RCNN style. Inherits AbstractDataLayer"""
+
+    def parse_param_str(self, param_str):
+        """Parse params"""
+        top_names_choices = ['input_image', 'roi', 'viewpoint', 'bbx_amodal', 'bbx_crop', 'center_proj']
+        default_mean_bgr = [103.0626238, 115.90288257, 123.15163084]  # ResNet
+        default_size_range = [600, 1024]
+
+        parser = argparse.ArgumentParser(description='Fast RCNN style Data Layer')
+        parser.add_argument("-m", "--mean_bgr", nargs=3, default=default_mean_bgr, type=float, metavar=('B', 'G', 'R'), help="Mean BGR color value")
+        parser.add_argument("-i", "--imgs_per_batch", default=2, type=int, help="Images per batch")
+        parser.add_argument("-r", "--rois_per_image", default=64, type=int, help="ROIs per image. Use -1 for dynamic number of rois (e.g during test time)")
+        parser.add_argument("-t", "--top_names", nargs='+', choices=top_names_choices, required=True,
+                            type=str, help="ordered list of top names e.g input_image azimuth shape")
+        parser.add_argument("-c", "--crop_target", default='bbx_visible',
+                            choices=['bbx_amodal', 'bbx_visible'], type=str, help="bbx type used for cropping (Defaults to bbx_visible)")
+        parser.add_argument("-sr", "--size_range", nargs=2, default=default_size_range, type=int, 
+                            metavar=('LOW', 'HI'), help="upper and lower bounds for length of shorter size of image in pixels")
+        parser.add_argument("-sm", "--size_max", default=3072, type=int,  help="Max pixel size of the longest side of a scaled input image")
+        parser.add_argument("-f", "--flip_ratio", default=0.5, type=float, help="Flip ratio in range [0, 1] (Defaults to 0.5)")
+        parser.add_argument("-j", "--jitter_iou_min", default=0.7, type=float, help="Minimum jitter IOU for crop_target")
+
+        params = parser.parse_args(param_str.split())
+
+        print "---------------- FastRCNNDataLayer Config ----------------------"
+        for arg in vars(params):
+            print "\t{} \t= {}".format(arg, getattr(params, arg))
+        print "------------------------------------------------------------"
+
+        return params
+
+    def setup(self, bottom, top):
+        print "Setting up FastRCNNDataLayer ..."
+
+        # params is expected as argparse style string
+        params = self.parse_param_str(self.param_str)
+
+        # Store the ordered list of top_names
+        self.top_names = params.top_names
+        # Store imgs_per_batch
+        self.imgs_per_batch = params.imgs_per_batch
+        # Store rois_per_image
+        self.rois_per_image = params.rois_per_image
+        # create mean bgr
+        self.mean_bgr = np.array(params.mean_bgr)
+        # set crop_target
+        self.crop_target = params.crop_target
+        # set size_range
+        self.size_range = params.size_range
+        # set size_max
+        self.size_max = params.size_max
+        # set flip_ratio
+        self.flip_ratio = params.flip_ratio
+         # set jitter_iou_min
+        self.jitter_iou_min = params.jitter_iou_min
+
+        assert len(top) == len(self.top_names), "Number of tops do not match specified top_names"
+
+        assert self.size_range[1] >= self.size_range[0], "invalid size_range = {}".format(self.size_range)
+        assert self.size_max >= self.size_range[1], "size_max needs to be greater than max shorter size"
+
+        assert 0.0 < self.jitter_iou_min <= 1.0, "jitter_iou_min needs to be in [0, 1], but got {}".format(self.jitter_iou_min)        
+
+        rois_per_batch = max(2 * self.rois_per_image, 1)
+
+        # set top shapes
+        top_shapes = {
+            "input_image": (self.imgs_per_batch, 3, 768, 1024),   # Use dummy H W
+            "roi": (rois_per_batch, 5),
+            "viewpoint": (rois_per_batch, 3),
+            "bbx_amodal": (rois_per_batch, 4),
+            "bbx_crop": (rois_per_batch, 4),
+            "center_proj": (rois_per_batch, 2),
+        }
+
+        # Reshape tops
+        for top_index, top_name in enumerate(self.top_names):
+            top[top_index].reshape(*top_shapes[top_name])
+
+        # Create a loader to load the images.
+        self.image_loader = BatchImageLoader(transpose=False)
+
+        # Create placeholder for annotations
+        self.data_samples = []
+
+        print 'RCNNDataLayer has been setup.'
+
+    def add_dataset(self, dataset):
+        """Add annotations from a json dataset"""
+        print '---- Adding data from {} datatset -----'.format(dataset.name())
+        print 'Number of data points (annotations) = {:,}'.format(len(self.data_samples))
+
+        image_files = [osp.join(dataset.rootdir(), image_info['image_file']) for image_info in dataset.annotations()]
+        self.image_loader.preload_images(image_files)
+        self.data_samples.extend(dataset.annotations())
+
+        assert len(self.data_samples) == len(self.image_loader), "len(self.data_samples) = {} while len(self.image_loader) = {}".format(
+            len(self.data_samples), len(self.image_loader))
+
+        self.image_loader.preload_images(image_files)
+        print 'Number of data points (annotations) = {:,}'.format(len(self.data_samples))
+        print "--------------------------------------------------------------------"
+
+    def generate_datum_ids(self):
+        """generate data ids"""
+        num_of_data_points = len(self.data_samples)
+
+        # set of data indices in [0, num_of_data_points)
+        self.data_ids = range(num_of_data_points)
+        self.curr_data_ids_idx = 0
+
+        assert len(self.data_ids) == num_of_data_points
+
+        # Shuffle from the begining if in the train phase
+        if self.phase == caffe.TRAIN:
+            shuffle(self.data_ids)
+
+        assert len(self.data_ids) > self.imgs_per_batch, 'imgs_per_batch ({})is smaller than total number of images ({}).'.format(
+            self.imgs_per_batch, len(self.data_ids))
+        print 'Total number of data points (annotations) = {:,}'.format(num_of_data_points)
+        return num_of_data_points
+
+    def forward(self, bottom, top):
+        """
+        Load current batch of data and labels to caffe blobs
+        """
+
+        assert hasattr(self, 'data_ids'), 'Most likely data has not been initialized before calling forward()'
+
+        image_ids = []
+        for _ in xrange(self.imgs_per_batch):
+            # Did we finish an epoch?
+            if self.curr_data_ids_idx == len(self.data_ids):
+                self.curr_data_ids_idx = 0
+                shuffle(self.data_ids)
+            
+            # Current Data index
+            img_idx = self.data_ids[self.curr_data_ids_idx]
+            image_ids.append(img_idx)
+            
+            self.curr_data_ids_idx += 1
+        
+        mb_data = self.prepare_mini_batch(image_ids)
+        
+    def prepare_mini_batch(self, image_ids):
+        img_blob, img_scales, img_flippings = self.prepare_image_blob(image_ids)
+
+        obj_blobs = self.pepare_object_blobs(image_ids, img_scales, img_flippings)
+        
+        mb_data = {}
+        mb_data['input_image'] = img_blob
+        return mb_data
+
+    def pepare_object_blobs(self, image_ids, img_scales, img_flippings):
+        num_images = len(image_ids)
+        for i in xrange(num_images):
+            obj_infos = self.data_samples[image_ids[i]]['objects']
+            obj_infos = sample_object_infos(obj_infos, self.rois_per_image, self.jitter_iou_min)
+
+
+
+
+    
+    def prepare_image_blob(self, image_ids):
+        # returns image blob and also scaling, flipping params for each image
+        num_images = len(image_ids)
+        images = []
+        img_scales = []
+        img_flippings = []
+        for i in xrange(num_images):
+            img = self.image_loader[image_ids[i]].astype(np.float32) - self.mean_bgr
+            assert img.ndim == 3 and img.shape[2] == 3
+
+            flip = True if random() < self.flip_ratio else False
+            target_size = randint(self.size_range[0], self.size_range[1])
+
+            if flip:
+                img = img[:, ::-1, :]
+
+            img_hw = img.shape[:2]
+            img_size_min = np.min(img_hw)
+            img_size_max = np.max(img_hw)
+            img_scale = float(target_size) / float(img_size_min)
+            # Prevent the biggest axis from being more than MAX_SIZE
+            if np.round(img_scale * img_size_max) > self.size_max:
+                img_scale = float(self.size_max) / float(img_size_max)
+            #resize img by img_scale
+            img = cv2.resize(img, None, None, fx=img_scale, fy=img_scale, interpolation=cv2.INTER_LINEAR)
+            
+            images.append(img)
+            img_scales.append(img_scale)
+            img_flippings.append(flip)
+        
+        max_hw = np.array([img.shape[:2] for img in images]).max(axis=0)
+        img_blob = np.zeros((num_images, max_hw[0], max_hw[1], 3), dtype=np.float32)
+
+        for i in xrange(num_images):
+            img = images[i]
+            img_blob[i, 0:img.shape[0], 0:img.shape[1], :] = img
+        
+        # Make axis order NCHW
+        img_blob = img_blob.transpose((0, 3, 1, 2))
+
+        return img_blob, img_scales, img_flippings
+
+    def sample_object_infos(object_infos, number_of_objects, jitter_iou_min):
+        sample_object_infos = []
+        while len(sample_object_infos) < number_of_objects:
+            
