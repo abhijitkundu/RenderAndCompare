01567471bf96965f9b58af0e16809facef5555fc
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-01 14:26:36 -0400

    Adding some simple dataset utils functions

--- python/RenderAndCompare/datasets/__init__.py
+++ python/RenderAndCompare/datasets/__init__.py
@@ -5,5 +5,6 @@ Geometry sub-module of RenderAndCompare
 from .dataset import Dataset, NoIndent
 from .image_loaders import *
 from .kitti_helper import *
+from .dataset_utils import *
 
-__all__ = ['dataset', 'image_loaders', 'kitti_helper']
+__all__ = ['dataset', 'image_loaders', 'kitti_helper', 'dataset_utils']
---
+++ python/RenderAndCompare/datasets/dataset_utils.py
@@ -0,0 +1,44 @@
+"""
+Some usefule dataset related functionality
+"""
+
+from random import shuffle
+import numpy as np
+import cv2
+from RenderAndCompare.geometry import create_jittered_bbx, bbx_iou_overlap
+
+def sample_object_infos(object_infos, number_of_objects, jitter_iou_min):
+    """Sample number_of_objects object infos from object_infos by jittering"""
+    sampled_object_infos = []
+    number_of_gt_objects = len(object_infos)
+    assert number_of_gt_objects, "Cannot have 0 objects"
+    obj_ids = range(number_of_gt_objects)
+    shuffle(obj_ids)
+    i = 0
+    while len(sampled_object_infos) < number_of_objects:
+        if i >= number_of_gt_objects:
+            shuffle(obj_ids)
+            i = 0
+        obj_id = obj_ids[i]
+        i += 1
+        bbx_crop_gt = np.array(object_infos[obj_id]['bbx_visible'])
+        while True:
+            bbx_crop = create_jittered_bbx(bbx_crop_gt, jitter_iou_min)
+            max_iou_obj_id = np.asarray([bbx_iou_overlap(bbx_crop, np.array(object_infos[j]['bbx_visible'])) for j in xrange(number_of_gt_objects)]).argmax()
+            if max_iou_obj_id == obj_id:
+                break
+
+        obj_info = object_infos[obj_id].copy()
+        obj_info['bbx_crop'] = bbx_crop
+        sampled_object_infos.append(obj_info)
+    return sampled_object_infos
+
+def draw_bbx2d(image, boxes, color=(0, 255, 0), thickness=1, copy=True):
+    if copy:
+        image_ = image.copy()
+    else:
+        image_ = image
+    for box in boxes:
+        bbx = np.array(box, dtype=np.float32)
+        cv2.rectangle(image_, tuple(bbx[:2]), tuple(bbx[2:]), color, thickness)
+    return image_
