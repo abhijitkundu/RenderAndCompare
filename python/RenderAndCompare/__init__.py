20c163034bd946973e799f080134d5aa2d5328ec
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-03 16:11:39 -0400

    Added submodules import

--- python/RenderAndCompare/__init__.py
+++ python/RenderAndCompare/__init__.py
@@ -2,4 +2,9 @@
 RenderAndCompare module
 """
 
+from . import datasets
+from . import evaluation
+from . import geometry
+from . import prediction
+
 __all__ = ['datasets', 'prediction', 'geometry', 'layers', 'evaluation']
