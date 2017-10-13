d1255eedaa1f3ef460c5d4e52796eef6b900c3b5
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-12 21:12:37 -0400

    renamed dataset.py --> image_dataset.py

--- python/RenderAndCompare/datasets/__init__.py
+++ python/RenderAndCompare/datasets/__init__.py
@@ -2,9 +2,9 @@
 Geometry sub-module of RenderAndCompare
 """
 
-from .dataset import Dataset, NoIndent
+from .image_dataset import Dataset, NoIndent
 from .image_loaders import *
 from .kitti_helper import *
 from .dataset_utils import *
 
-__all__ = ['dataset', 'image_loaders', 'kitti_helper', 'dataset_utils']
+__all__ = ['image_dataset', 'image_loaders', 'kitti_helper', 'dataset_utils']
